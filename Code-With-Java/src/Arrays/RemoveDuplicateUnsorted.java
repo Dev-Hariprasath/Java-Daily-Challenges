cd2b9cdf1590afe683bfb29315ea6eec9404cb55
hariprasath-accenous <hariprasath0620@gmail.com>
2025-10-15 12:22:22 +0530

    Day 05 Completed

---
+++ Code-With-Java/src/Arrays/RemoveDuplicateUnsorted.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicateUnsorted {
+    public static void main(String[] args) {
+        int[] arr = {9, 8, 7, 6, 5, 1, 4, 7, 8, 2};
+
+        Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.toSet())
+                .forEach(System.out::print);
+    }
+}
---
+++ Code-With-Java/src/Arrays/RemoveDuplicatesSorted.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesSorted {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 1, 4, 5, 9};
+        Arrays.stream(arr)
+                .boxed()
+                .distinct()
+                .forEach(System.out::println);
+    }
+}
