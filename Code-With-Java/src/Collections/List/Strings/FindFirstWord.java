967ac21838b2eefd2e8c6106585bea5a0c9e9be6
hariprasath-accenous <hariprasath0620@gmail.com>
2025-10-18 09:51:17 +0530

    Day 08 Completed

---
+++ Code-With-Java/src/Collections/List/Strings/FindFirstWord.java
@@ -0,0 +1,17 @@
+package Collections.List.Strings;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class FindFirstWord {
+    public static void main(String[] args) {
+        List<String> st = Arrays.asList("hari", "prasath", "ganesh", "ram", "mogan");
+
+       Optional<String> str = st.stream()
+                .filter(s -> s.startsWith("h"))
+                .findFirst();
+        System.out.println(str.isPresent() ? str.get() : "No Words");
+
+    }
+}
