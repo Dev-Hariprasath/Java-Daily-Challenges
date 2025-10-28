1c5160d556f4393fdf9d54308e73d1aab684c87a
hariprasath-accenous <hariprasath0620@gmail.com>
2025-10-28 12:02:10 +0530

    Day 17 Completed

---
+++ Code-With-Java/src/Collections/List/Strings/GroupByLength.java
@@ -0,0 +1,17 @@
+package Collections.List.Strings;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GroupByLength {
+    public static void main(String[] args) {
+        List<String> st = Arrays.asList("hari", "prasath", "ganesh", "ram",
+                "mogan", "hari", "prasath", "ganesh", "xyz", "hari", "harini");
+
+        System.out.println("Key - Values");
+        st.stream()
+                .collect(Collectors.groupingBy(String::length))
+                .forEach((k, v) -> System.out.println(k + " - " + v));
+    }
+}
