70859f587a95b3e15d033fc7fb425120f657882a
hariprasath-accenous <hariprasath0620@gmail.com>
2025-10-29 11:28:13 +0530

    Day  19 Completed

---
+++ Code-With-Java/src/Strings/FrequencyCharacter.java
@@ -0,0 +1,16 @@
+package Strings;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FrequencyCharacter {
+    public static void main(String[] args) {
+        String str = "hariprasath";
+        System.out.println("Character - Frequency");
+        str.chars()
+                .mapToObj( c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(),
+                         Collectors.counting()))
+                .forEach((k, v) -> System.out.println(k + " - " + v));
+    }
+}
---
+++ Code-With-Java/src/Strings/Palindrome.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class Palindrome {
+    public static void main(String[] args) {
+
+        String s = "abbac";
+        String r = "";
+
+        for(int i = s.length() - 1; i >= 0; i--){
+            r += s.charAt(i);
+        }
+
+        System.out.println(s.equals(r) ? "Palindrome" : "Not a Palindrome");
+    }
+}
