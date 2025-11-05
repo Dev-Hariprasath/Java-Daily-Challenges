85527ecd49237bf68626b9725ea14c91122045ad
hariprasath-accenous <hariprasath0620@gmail.com>
2025-11-05 11:54:31 +0530

    Day 21 Completed

---
+++ Code-With-Java/src/Strings/FrequencyString.java
@@ -0,0 +1,22 @@
+package Strings;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FrequencyString {
+    public static void main(String[] args) {
+        String str = "babacccdeeeeefffffff";
+        Map<Character, Long> frq = str.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        String result = "";
+
+        for(char c : frq.keySet()){
+            result += c;
+            result += frq.get(c);
+        }
+        System.out.println("The frequency String is " +result);
+    }
+}
