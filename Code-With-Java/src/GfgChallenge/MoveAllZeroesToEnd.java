ac64b9c22c69b37c8e500ee47eb7a3d8906e2f45
hariprasath-accenous <hariprasath0620@gmail.com>
2025-11-08 12:14:40 +0530

    Gfg Day 1 & 2 Completed

---
+++ Code-With-Java/src/GfgChallenge/MoveAllZeroesToEnd.java
@@ -0,0 +1,17 @@
+package GfgChallenge;
+
+public class MoveAllZeroesToEnd {
+    void pushZerosToEnd(int[] arr) {
+        int index = 0;
+
+        for(int n : arr){
+            if(n != 0){
+                arr[index++] = n;
+            }
+        }
+
+        while(index < arr.length){
+            arr[index++] = 0;
+        }
+    }
+}
---
+++ Code-With-Java/src/GfgChallenge/SecondLargest.java
@@ -0,0 +1,19 @@
+package GfgChallenge;
+
+public class SecondLargest {
+    public int getSecondLargest(int[] arr) {
+        int first_Max = Integer.MIN_VALUE;
+        int second_Max = Integer.MIN_VALUE;
+
+        for(int n : arr){
+            if(n > first_Max){
+                second_Max = first_Max;
+                first_Max = n;
+            }else if (n > second_Max && n != first_Max){
+                second_Max = n;
+            }
+        }
+
+        return second_Max == Integer.MIN_VALUE ? -1 : second_Max;
+    }
+}
